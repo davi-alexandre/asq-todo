576989ff644f9b471f8794ba2e910b7e87d6cc35
Davi Alexandre <deltreertl@gmail.com>
2023-03-04 19:35:22 -0300

    base config

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="p-4 text-black font-bold bg-orange-200">
+      TailwindCSS
+      <p>
+        SASS
+      </p>
+    </div>
+  );
+}
